bec01a108918e7ad35ce056877cb107753e552b3
meng eric <outfoxed@163.com>
2020-06-16 15:05:09 +0800

    midify MyUser

---
+++ Infrastructure/Identity/Migrations/20200616070051_ModifyMyUser.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Identity.Migrations
+{
+    public partial class ModifyMyUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAdmin",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "AspNetUsers",
+                maxLength: 255,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAdmin",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "AspNetUsers");
+        }
+    }
+}
--- Infrastructure/Identity/MyUser.cs
+++ Infrastructure/Identity/MyUser.cs
@@ -1,8 +1,15 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Infrastructure.Identity
 {
     public class MyUser : IdentityUser<int>
     {
+        [DefaultValue(false)]
+        public bool IsAdmin { get; set; }
+
+        [MaxLength(255)]
+        public string Name { get; set; }
     }
 }
