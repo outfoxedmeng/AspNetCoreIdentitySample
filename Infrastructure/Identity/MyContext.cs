b68ddf075bafcc6b7cc956d8ea3c38c5c64b5644
meng eric <outfoxed@163.com>
2020-06-16 10:42:52 +0800

    添加项目文件。

---
+++ Infrastructure/Identity/MyContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Identity
+{
+    public class MyContext : IdentityDbContext<MyUser, IdentityRole<int>, int>
+    {
+    }
+}
